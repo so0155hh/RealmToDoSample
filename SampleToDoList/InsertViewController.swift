4b2fc02df9078df3e5bf4ac8cdb3ce76b978096c
Nozomi <nozomi.kuwahara@gmail.com>
2020-02-23 09:47:58 +0900

    ToDoSmapleForRealm

---
+++ SampleToDoList/InsertViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InsertViewController.swift
+//  SampleToDoList
+//
+//  Created by 桑原望 on 2020/02/22.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import UIKit
+
+class InsertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
